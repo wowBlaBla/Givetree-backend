904ff7f9f00a4cc1d4a16a53303a6be5dfe9606f
dezzzus <alex_dorn@mail.com>
2022-09-28 02:32:01 +0800

    update register endpoint issue

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: ["content-type", "authorization"],
+    origin: "http://localhost:3000",
+    credentials: true,
+  });
   app.setGlobalPrefix("api");
 
   const config = new DocumentBuilder()
@@ -15,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
