5ffb009c9a1861240f12016d3aef27e0792ba5c2
Dave Cartwright <hit.dave.cartwright@gmail.com>
2022-10-03 00:26:51 +1300

    added env var for cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,20 @@ import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('APP_SERVER_PORT') || 3000;
+  const app_env = configService.get<string>('APP_ENV') || "dev";
+  const cors_frontend_origin = configService.get<string>('CORS_FRONTEND_ORIGIN') || "*";
+
   app.enableCors({
-    origin: "*",
+    origin: cors_frontend_origin,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     credentials: true,
     optionsSuccessStatus: 204
   });
   app.setGlobalPrefix("api");
-
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('APP_SERVER_PORT') || 3000;
-  const app_env = configService.get<string>('APP_ENV') || "dev";
-
+  
   const config = new DocumentBuilder()
     .setTitle("GiveTree API")
     .setDescription("API for the GiveTree.")
