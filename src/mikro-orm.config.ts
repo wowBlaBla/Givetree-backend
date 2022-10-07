4f23bae08636ed64aed7f109d9f2212ce349bc63
dezzzus <alex_dorn@mail.com>
2022-10-07 22:32:07 +0800

    update migration

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -4,14 +4,14 @@ import { SqlHighlighter } from "@mikro-orm/sql-highlighter";
 import { ConfigService } from "@nestjs/config";
 
 const logger = new Logger("MikroORM");
-const configService = new ConfigService( { envFilePath: '.env' });
+const configService = new ConfigService({ envFilePath: ".env" });
 const config = {
-  type: configService.get<string>('DB_TYPE') || "mariadb",
-  host: configService.get<string>('DB_HOST') || 'localhost',
-  port: configService.get<number>('DB_PORT')|| 3360,
-  user: configService.get<string>('DB_USER') || "root",
-  password: configService.get<string>('DB_PASSWORD') || "mariadb",
-  dbName: configService.get<string>('DB_NAME') || "mariadb",
+  type: configService.get<string>("DB_TYPE") || "mariadb",
+  host: configService.get<string>("DB_HOST") || "localhost",
+  port: configService.get<number>("DB_PORT") || 3360,
+  user: configService.get<string>("DB_USER") || "root",
+  password: configService.get<string>("DB_PASSWORD") || "",
+  dbName: configService.get<string>("DB_NAME") || "mariadb",
   entities: ["./dist/**/*.entity.js"],
   entitiesTs: ["./src/**/*.entity.ts"],
   debug: true,
@@ -23,4 +23,3 @@ const config = {
 } as Options;
 
 export default config;
-
