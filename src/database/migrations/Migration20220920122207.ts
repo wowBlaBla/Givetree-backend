8e1e2c07632cc0ce18dbd9737a6bd1f9de8abb60
wowBlaBla <wow1992122@gmail.com>
2022-10-29 06:04:44 +0300

    update migration

--- src/database/migrations/Migration20220920122207.ts
+++ src/database/migrations/Migration20220920122207.ts
@@ -22,12 +22,16 @@ export class Migration20220920122207 extends Migration {
       "create table `charity_property` (`id` int unsigned not null auto_increment primary key, `created_at` datetime not null, `updated_at` datetime not null, `founded_at` datetime, `employee` int(255) unsigned default 0, `founders` text(2000), `phone` varchar(255), `user_id` int(11) unsigned not null) default character set utf8mb4 engine = InnoDB;"
     );
 
-    this.addSql(
-      "create table `socials` (`id` int unsigned not null auto_increment primary key, `created_at` datetime not null, `updated_at` datetime not null, `social` varchar(255) not null, `link` text(2000) not null, `user_id` int(11) unsigned, `collection_id` int(11) unsigned, `item_type` enum('user', 'collection') not null) default character set utf8mb4 engine = InnoDB;"
-    );
+    // this.addSql(
+    //   "create table `socials` (`id` int unsigned not null auto_increment primary key, `created_at` datetime not null, `updated_at` datetime not null, `social` varchar(255) not null, `link` text(2000) not null, `user_id` int(11) unsigned, `collection_id` int(11) unsigned, `item_type` enum('user', 'collection') not null) default character set utf8mb4 engine = InnoDB;"
+    // );
 
     this.addSql(
       "create table `collections` (`id` int unsigned not null auto_increment primary key, `created_at` datetime not null, `updated_at` datetime not null, `address` varchar(255), `name` varchar(255), `pattern` varchar(255), `description` text(4000), `theme` enum('padded', 'contained', 'covered') not null default 'contained', `logo` text(2000), `featured` text(2000), `banner` text(2000), category enum('art','collectible','music','photography','sport','trading','utility')) default character set utf8mb4 engine = InnoDB;" 
-    )
+    );
+
+    this.addSql(
+      "create table `sales` (`id` int unsigned not null auto_increment primary key, `created_at` datetime not null, `updated_at` datetime not null, `collection` varchar(255) not null, `token_id` int unsigned not null, `seller` varchar(255) not null) default character set utf8mb4 engine = InnoDB;"
+    );
   }
 }
